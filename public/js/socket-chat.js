d9fc59a6247628c3033b602ce285c655a23aae5b
Gustavo RM <gustavo.rm.isc@gmail.com>
2023-02-17 00:15:17 -0600

    Connect an user from the frontend

---
+++ public/js/socket-chat.js
@@ -0,0 +1,50 @@
+
+//Client socket
+const socket = io();
+
+//Params from the URL
+const params = new URLSearchParams( window.location.search );
+
+//If there is no name in the url then redirect to the index.html
+if( !params.has('name') ){
+    window.location = 'index.html';
+    throw new Error( 'The user name is required' );
+}
+
+//Get the name if it exists in the url
+const user = {
+    name: params.get( 'name' )
+};
+
+//Listen when a users connects to the server
+socket.on('connect', () => {
+    console.log('Connected to the server');
+
+    //Send the user and receive the response from the server
+    socket.emit( 'enter-chat', user, ( res ) => {
+        console.log( 'Connected users', res );
+    });
+});
+
+// escuchar
+socket.on('disconnect', () => {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+socket.emit('send-message', {
+    user: 'Fernando',
+    message: 'Hello world!'
+}, function(resp) {
+    console.log('server response: ', resp);
+});
+
+// Escuchar información
+socket.on('send-message', (mensaje) => {
+
+    console.log('Server:', mensaje);
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,31 +1,27 @@
+import Users from "../models/users.js";
+
+const users = new Users();
 
 export const socketController = ( socket ) => {
 
     console.log('Client connected...', socket.id);
 
-    socket.emit('send-message', {
-        user: 'Admin',
-        message: 'Welcome to the app!'
-    });
+    //Listen when a user connects
+    socket.on( 'enter-chat', ( user, callback ) => {
 
-    socket.on( 'disconnect', () => {
-        console.log('Client disconnected...', socket.id);
-    });
-
-    /**
-     * This listens when a client emits a message and the callback is the things we want to do when the client emits the message
-     * The first arg of the callback is the payload
-     * Th second arg is a callback where we can send data to the client
-     */
-    socket.on( 'send-message', ( payload, callback ) => {
-
-        // console.log('Message from the "client" received: ', payload);
+        //If there is no name as a payload throw an error
+        if( !user.name ){
+            return callback({
+                error: true,
+                message: 'The name is required'
+            });
+        }
         
-        // const id = '123ABC';
-        // callback( id );
-        
-        //Sending a message to all the connected clients
-        socket.broadcast.emit( 'send-message', payload );
+        //If the name exists then add it to the list of connected users
+        let usersList = users.addUser( socket.id, user.name );
+
+        //Send back the list of users
+        callback( usersList );
 
     });
     
